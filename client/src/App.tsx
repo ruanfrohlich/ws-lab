b480b527c374985cacbeca017c9a9442f45cc5ea
Ruan Frohlich <ruanfrohlich@gmail.com>
2025-01-24 22:42:20 -0300

    Updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useLayoutEffect, useRef, useState } from 'react';
+import { FormEvent, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { w3cwebsocket } from 'websocket';
 
 interface ISocketData {
@@ -12,6 +12,8 @@ interface IState {
   connected: boolean;
   username?: string;
   message?: string;
+  error?: Error;
+  hideChat?: boolean;
   receivedMessages: Array<{
     id: string;
     avatarColor?: string;
@@ -23,13 +25,22 @@ interface IState {
 }
 
 export function App() {
+  const formRef = useRef<HTMLFormElement>(null);
+  const chatRef = useRef<HTMLDivElement>(null);
+  const chatMessages = useRef<HTMLDivElement>(null);
   const [state, setState] = useState<IState>({
     connected: false,
     receivedMessages: [],
+    hideChat: false,
   });
 
-  const formRef = useRef<HTMLFormElement>(null);
-  const chatRef = useRef<HTMLDivElement>(null);
+  const isValid =
+    state.connected &&
+    state.username &&
+    state.username.length > 2 &&
+    state.receiver?.id &&
+    state.receiver.id.length > 2 &&
+    state.message;
 
   const generateRandomColor = () => {
     let maxVal = 0xffffff; // 16777215
@@ -55,7 +66,14 @@ export function App() {
   const webSocketHandler = () => {
     const wsClient = state.socket ?? new w3cwebsocket('ws://localhost:3001/');
 
-    wsClient.onerror = function () {
+    setState((state) => {
+      return {
+        ...state,
+        error: null,
+      };
+    });
+
+    wsClient.onerror = function (info) {
       console.log('Connection Error');
 
       setState((state) => {
@@ -63,6 +81,7 @@ export function App() {
           ...state,
           connected: false,
           socket: null,
+          error: info,
         };
       });
     };
@@ -91,12 +110,6 @@ export function App() {
       const data = JSON.parse(String(e.data)) as ISocketData;
 
       setState((state) => {
-        if (data.receiverId !== state.username) {
-          return {
-            ...state,
-          };
-        }
-
         const oldMessages = state.receivedMessages.find(
           (user) => user.id === data.senderId,
         );
@@ -131,14 +144,45 @@ export function App() {
     };
   };
 
+  const hideChat = () => {
+    const chatEl = chatRef.current;
+
+    if (!chatEl) return;
+
+    setState((state) => {
+      return {
+        ...state,
+        hideChat: !state.hideChat,
+      };
+    });
+
+    const chatSize = chatEl.getBoundingClientRect().height;
+
+    if (state.hideChat) {
+      chatEl.style.transform = `translateY(${chatSize - 30}px)`;
+    } else {
+      chatEl.style.transform = `translateY(0)`;
+    }
+  };
+
+  useEffect(() => {
+    const messages = chatMessages.current;
+
+    if (messages) {
+      messages.scrollTop = messages.scrollHeight;
+    }
+  }, [state.receivedMessages]);
+
   useLayoutEffect(() => {
     let timer: NodeJS.Timeout;
+    let reconnectionCount = 0;
 
     (async () => {
-      while (!state.connected) {
+      while (!state.connected && reconnectionCount <= 3) {
         await new Promise((res) => {
           timer = setTimeout(() => {
             webSocketHandler();
+            reconnectionCount++;
             res('ok');
           }, 3000);
         });
@@ -156,12 +200,14 @@ export function App() {
       <p>
         Você está atualmente{' '}
         {state.connected ? (
-          <span className='text-green-600'>conectado</span>
+          <span className='text-green-600'>conectado(a)</span>
         ) : (
-          <span className='text-red-600'>desconectado</span>
+          <span className='text-red-600'>desconectado(a)</span>
         )}
         .
       </p>
+
+      {/* FORM */}
       <form className='my-4 border border-black p-3' noValidate ref={formRef}>
         <div className='flex flex-col gap-[20px] mb-3'>
           <div className='flex flex-col'>
@@ -220,45 +266,62 @@ export function App() {
         </div>
         <button
           type='submit'
-          className='rounded-full uppercase bg-blue-500 hover:bg-slate-600 transition-all duration-100 text-white px-3 py-1 text-[14px]'
+          className={`rounded-full disabled:bg-gray-500 uppercase bg-blue-500 hover:bg-slate-600 transition-all duration-100 text-white px-3 py-1 text-[14px] ${!isValid && 'pointer-events-none'}`}
           onClick={handleClick}
+          disabled={!isValid}
         >
           Enviar mensagem
         </button>
       </form>
 
+      {/* CHATBOX */}
       <div
         ref={chatRef}
-        className='chat fixed z-[99999] bottom-0 left-[50%] translate-x-[-50%] w-full max-w-[400px] h-[300px] overflow-y-scroll border-t border-r border-l border-black rounded-t-lg'
+        className='fixed z-[99999] bottom-0 right-[20px] w-full max-w-[400px] h-[300px] overflow-hidden border-t border-r border-l border-black rounded-t-lg transition-transform duration-200'
       >
-        {state.receivedMessages.map((el, i) => (
-          <div
-            key={i}
-            className={`flex gap-[15px] py-1 px-4 ${el.id === state.username && 'flex-row-reverse'}`}
-          >
-            <div className='flex flex-col items-center'>
-              <span
-                style={{
-                  backgroundColor: el.avatarColor,
-                }}
-                className={`rounded-full flex justify-center items-center text-lg font-bold p-2 w-[60px] h-[60px]`}
-              >
-                {el.id.charAt(0)}
-              </span>
-              <p>{el.id}</p>
-            </div>
-            <div className='flex flex-col items-start'>
-              {el.messages.map((message, index) => (
-                <p
-                  key={index}
-                  className={` mb-2 rounded-ss-xl rounded-se-xl ${el.id === state.username ? 'rounded-bl-xl' : 'rounded-br-xl'} bg-slate-500 text-white p-2`}
+        <div className='absolut t-0 bg-slate-900'>
+          <button className='text-white w-full h-[30px]' onClick={hideChat}>
+            <span>Chat</span>
+          </button>
+        </div>
+        <div
+          ref={chatMessages}
+          className='p-3 h-[270px] overflow-y-scroll overflow-x-hidden'
+        >
+          {state.receivedMessages.map((el, i) => (
+            <div
+              key={i}
+              className={`chat-message flex gap-[15px] py-1 px-4 ${el.id === state.username && 'flex-row-reverse'}`}
+            >
+              <div className='flex flex-col items-center'>
+                <span
+                  style={{
+                    backgroundColor: el.avatarColor,
+                  }}
+                  className={`relative rounded-full flex justify-center items-center text-lg font-bold p-2 w-[60px] h-[60px]`}
                 >
-                  {message}
-                </p>
-              ))}
+                  {el.id === state.username && (
+                    <span className='absolute leading-4 px-2 shadow-sm t-0 left-[50%] translate-x-[-52%] translate-y-[-30px] line bg-green-700 text-white rounded text-[12px]'>
+                      Você
+                    </span>
+                  )}
+                  {el.id.charAt(0)}
+                </span>
+                <p>{el.id}</p>
+              </div>
+              <div className='flex flex-col items-start'>
+                {el.messages.map((message, index) => (
+                  <p
+                    key={index}
+                    className={`mb-2 rounded-ss-xl rounded-se-xl ${el.id === state.username ? 'rounded-bl-xl' : 'rounded-br-xl'} bg-slate-500 text-white p-2`}
+                  >
+                    {message}
+                  </p>
+                ))}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
